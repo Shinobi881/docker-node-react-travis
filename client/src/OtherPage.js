dba0b0cc5bbb6a4a7a1dd363bb6fea0ab588e34f
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2019-01-13 01:29:30 -0800

    (feat) finish client setup

---
+++ client/src/OtherPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const OtherPage = () => (
+  <div>
+    Im some other page!
+    <Link to="/">Go back home</Link>
+  </div>
+);
+
+OtherPage.displayName = 'OtherPage';
+
+export default OtherPage;
